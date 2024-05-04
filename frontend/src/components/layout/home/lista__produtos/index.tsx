55bed528128fe4262a624eb17e80b3b3b2df99b9
cairesdev <joaocaires.dev@gmail.com>
2024-05-04 20:27:20 -0300

    lista

--- frontend/src/components/layout/home/lista__produtos/index.tsx
+++ frontend/src/components/layout/home/lista__produtos/index.tsx
@@ -3,14 +3,14 @@ import ItemProduto from "./item__produto";
 import styles from "@/styles/public/produto.module.css";
 
 export default async function ListaProdutosHome() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
   const { results } = await getProducts();
 
   return (
     <main className={styles.home__produtos}>
       <section>
         <h3 className={styles.titulo__secao__lista}>Todos os Produtos</h3>
-        <div className={styles.container__itens}>
+        <div className={styles.container__itens_horizontal}>
           {results &&
             results.map((item) => <ItemProduto key={item._id} item={item} />)}
         </div>
@@ -20,9 +20,7 @@ export default async function ListaProdutosHome() {
 }
 
 export async function ListByCategory({ categoria }: { categoria: string }) {
-  const { results } = await getProductByCategoria({
-    categoria: categoria,
-  });
+  const { results } = await getProductByCategoria({ categoria });
 
   return (
     <main className={styles.home__produtos}>

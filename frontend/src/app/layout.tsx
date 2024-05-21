9605a583b735b0925e57d7c7e724360ac64425fc
cairesdev <joaocaires.dev@gmail.com>
2024-05-21 17:32:33 -0300

    META

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -51,6 +51,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
+        <meta
+          name="google-site-verification"
+          content="tv9OHVb6U6zIc9EjHP3NbhXsVdO5BG7xZa6b1-0FitU"
+        />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-1Z4N6RFL4T"
